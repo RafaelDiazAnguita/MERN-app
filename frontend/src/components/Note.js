553035a9bce786134978ff717924f6ca501380c3
RafaelDiazAnguita <55484240+RafaelDiazAnguita@users.noreply.github.com>
2022-03-08 13:42:41 +0100

    Conditional rendering

---
+++ frontend/src/components/Note.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Note = props => {
+    const { author,note } = props;
+
+    if(author == note.author){
+        
+    }
+    else{
+
+    }
+};
+
+export default Note;
--- frontend/src/components/NotesList.js
+++ frontend/src/components/NotesList.js
@@ -24,35 +24,57 @@ export default class NotesList extends Component {
   }
 
   renderPage = () => {
+    
     if(sessionStorage['logged']){
       return <div className='container mt-5'>
       <div className='row row-cols-3'>
         {
-          this.state.notes.map(note => (
-            <div className='col' key={note._id}>
-              <div className='custom-card py-3 px-4'>
+          this.state.notes.map(note => {
+            if(note.author == sessionStorage['username'])
+            return <div className='col' key={note._id}>
+            <div className='custom-card py-3 px-4'>
 
-                <div className='row'>
-                  <div className='col-2'></div>
-                  <div className='col-8'><h2 className='text-center mb-4'>{note.title}</h2></div>
-                  <div className='col-2'><Link className='btn btn-secondary' to={"/edit/" + note._id}>edit</Link></div>
-                </div>
-  
-                <h5>{note.author}</h5>
-                <p className='ms-2 note-content-view'>{note.content}</p>
-
-                <div className='row'>
-                  <div className='col-md-6 date-col'>
-                    <h6>{format(note.date)}</h6>
-                  </div>
-                  <div className='col-md-6 text-end'>
-                    <button className='btn btn-danger mt-4' onClick={() => this.deleteNote(note._id)}>X</button>
-                  </div>
-                </div>
-        
+              <div className='row'>
+                <div className='col-2'></div>
+                <div className='col-8'><h2 className='text-center mb-4'>{note.title}</h2></div>
+                <div className='col-2'><Link className='btn btn-secondary' to={"/edit/" + note._id}>edit</Link></div>
               </div>
+
+              <h5>{note.author}</h5>
+              <p className='ms-2 note-content-view'>{note.content}</p>
+
+              <div className='row'>
+                <div className='col-md-6 date-col'>
+                  <h6>{format(note.date)}</h6>
+                </div>
+                <div className='col-md-6 text-end'>
+                  <button className='btn btn-danger mt-4' onClick={() => this.deleteNote(note._id)}>X</button>
+                </div>
+              </div>
+      
             </div>
-          ))
+          </div>
+
+            else
+            return <div className='col' key={note._id}>
+            <div className='custom-card py-3 px-4'>
+
+              <div className='row'>
+                <div className='col-2'></div>
+                <div className='col-8'><h2 className='text-center mb-4'>{note.title}</h2></div>
+              </div>
+
+              <h5>{note.author}</h5>
+              <p className='ms-2 note-content-view'>{note.content}</p>
+
+              <div className='row'>
+                <div className='col-md-6 date-col'><h6>{format(note.date)}</h6></div>
+              </div>
+      
+            </div>
+          </div>
+            
+          })
         }
       </div>
       </div>
